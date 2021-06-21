bbde4a2a1da3ad7f3d93e17fa9ba5004224e44e8
simaranascimento <simara.c@thoughtworks.com>
2021-06-20 23:44:31 -0300

    inicia projeto react e integra com firebase

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from './components/Button'
+
+function App() {
+  return (
+   <div>
+    <Button text="bt1"/>
+    <Button text="bt2"/>
+    <Button text="bt3"/>
+    <Button text="bt4"/>
+    <Button text="bt5"/>
+    <Button text="bt6"/>
+   </div>
+  );
+}
+
+export default App;
